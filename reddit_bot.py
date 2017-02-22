fe070e9f06733a98166bad2672ccdd1f18dcd7e3
Cedricium <amaya.cedric@gmail.com>
2017-02-21 16:33:33 -0800

    v0.2 - Controlled Commenting Behavior
    In this version of the reddit bot, commenting behavior is controlled. Before, the bot would comment to any comment in a thread with the matching keyword multiple times. This behavior has been eliminated by storing the comment.id's the bot has already replied to in a file and reading this file before responding to comments again. Other changes include the correction of spelling errors and comments added to the source code for better readability.

--- reddit_bot.py
+++ reddit_bot.py
@@ -1,11 +1,16 @@
 import praw
 import time
+#import config
+	# config file looks like the following
+	# username = "<USERNAME>"
+	# password = "<PASSWORD>"
+	# client_id = "<CLIENT_ID> FROM REDDIT SCRIPT APP"
+import os
 
-# import config
-# config file looks like the following
-# username = "<USERNAME>"
-# password = "<PASSWORD>"
-# client_id = "<CLIENT_ID> FROM REDDIT SCRIPT APP"
+# global variables used throughout the program
+version = "v0.2"
+time_to_sleep = 10
+number_of_comments = 25
 
 def bot_login():
 	print "Logging in..."
@@ -15,15 +20,41 @@ def bot_login():
 			client_secret = config.client_secret,
 			user_agent = "Cedric's Commenting Test Bot - v0.1")
 	print "Logged in!"
+
 	return r
 
-def run_bot(r):
-	print "Obataining 25 comments..."
-	for comment in r.subreddit('cedriciumCSS').comments(limit=25):
-		if "bot" in comment.body:
+def run_bot(r, comments_replied_to):
+	print "Obtaining " + str(number_of_comments) + " comments..."
+
+	for comment in r.subreddit('cedriciumCSS').comments(limit = number_of_comments):
+		if "bot" in comment.body and comment.id not in comments_replied_to and comment.author != r.user.me():
 			print "String Found!"
 			comment.reply("I found the string, give me a treat!")
 			print "Replied to comment " + comment.id
+            
+            # add comment id replied to to the list
+			comments_replied_to.append(comment.id)
+
+			with open("comments_replied_to.txt", "a") as f:
+				f.write(comment.id + "\n")
+
+	print "Sleeping for " + str(time_to_sleep) + " seconds..."
+	# sleep for str(time_to_sleep) seconds before running bot again
+	time.sleep(time_to_sleep)
+
+# method checks for a file with comment.id's, creates one if not found
+def get_saved_comments():
+	if not os.path.isfile("comments_replied_to.txt"):
+		comments_replied_to = []
+	else:
+		with open("comments_replied_to.txt", "r") as f:
+			comments_replied_to = f.read()
+			comments_replied_to = comments_replied_to.split("\n")
+
+	return comments_replied_to
 
 r = bot_login()
-run_bot(r)
+comments_replied_to = get_saved_comments()
+
+while True:
+	run_bot(r, comments_replied_to)
