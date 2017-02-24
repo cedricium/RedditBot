01e9ed5f9f067bd270721bf70e5669a7d366a633
Cedricium <amaya.cedric@gmail.com>
2017-02-24 00:12:51 -0800

    Fix typos in config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 
 username = "<BOT REDDIT USERNAME>"
 password = "<BOT REDDIT PASSWORD>"
-client_id = "<CLIENT_ID - FOUND IN 'reddit.com/prefs/apps'>"
-client_secret = "<CLIENT_SECRET - FOUND IN 'reddit.com/prefs/apps'>"
+client_id = "<CLIENT_ID> - FOUND IN 'reddit.com/prefs/apps'"
+client_secret = "<CLIENT_SECRET> - FOUND IN 'reddit.com/prefs/apps'"
