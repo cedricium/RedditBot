8eddc24d77cd065809b9526718a2b36ee632751a
Cedricium <amaya.cedric@gmail.com>
2017-02-25 09:28:43 -0800

    Update bash script to include adding comments_replied_to.txt

--- push_to_github.sh
+++ push_to_github.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-commit_message="Update docs/index.md with new saved links."
+commit_message="Update docs/index.md with new saved links and comments replied to."
 
-git add docs/index.md
+git add comments_replied_to.txt docs/index.md
 git commit -m "$commit_message"
 git push origin master
