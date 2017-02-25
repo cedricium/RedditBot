5b829d1b02bc3aab7f001b7a5b2740e94dce4a04
Cedricium <amaya.cedric@gmail.com>
2017-02-24 23:10:44 -0800

    Add example config file

---
+++ config_example.py
@@ -0,0 +1,6 @@
+# replace the following values with your corresponding information
+
+username = "<BOT REDDIT USERNAME>"
+password = "<BOT REDDIT PASSWORD>"
+client_id = "<CLIENT_ID> - FOUND IN 'reddit.com/prefs/apps'"
+client_secret = "<CLIENT_SECRET> - FOUND IN 'reddit.com/prefs/apps'"
